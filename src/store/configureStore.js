3484d5f153cee5ec90cb79222ba9e25712dd1055
migot <florian.migot@ext.quicksign.com>
2017-01-04 11:41:02 +0100

    Added Redux extension for chrome

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
-import rootSaga from '../sagas'; // TODO: Next step
+import rootSaga from '../sagas';
 
 //  Returns the store instance
 // It can  also take initialState argument when provided
 const configureStore = () => {
-  const sagaMiddleware = createSagaMiddleware(); 
+  const sagaMiddleware = createSagaMiddleware();
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
   return {
-    ...createStore(rootReducer,
-      applyMiddleware(sagaMiddleware)),
+    ...createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware))),
     runSaga: sagaMiddleware.run(rootSaga)
   };
 };
